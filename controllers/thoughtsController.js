3453f3cdd4d5f68c9546401230640455c76a31fe
ML <labrecquemegan@hotmail.com>
2022-03-06 17:26:53 -0500

    finished models

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -2,14 +2,59 @@ const {Thought, User } = require('../models');
 
 const thoughtController = {
     // get all thoughts
+    getThought(req, res) {
+        Thought.find()
+          .then((thoughts) => res.json(thoughts))
+          .catch((err) => res.status(500).json(err));
+      },
 
     // get single thought
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+          .select('-__v')
+          .then((thought) =>
+            !thought
+              ? res.status(404).json({ message: 'No thought with that ID' })
+              : res.json(thought)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
 
     // create new thought
+    createThought(req, res) {
+        Thought.create(req.body)
+          .then((thought) => res.json(thought))
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
 
     // update a thought
-
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        )
+          .then((thought) =>
+            !thought
+              ? res.status(404).json({ message: 'No thought with this id!' })
+              : res.json(thought)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
+    
     // delete thought
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+          .then((thought) =>
+            !thought
+              ? res.status(404).json({ message: 'No thought with that ID' })
+              : res.json({ thought })
+              )
+          .catch((err) => res.status(500).json(err));
+      },
 
     // add reaction to thoughts
 
