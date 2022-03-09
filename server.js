2098af1392614b60fcd2e74094b23b4cbdfbce10
ML <labrecquemegan@hotmail.com>
2022-03-09 16:41:16 -0500

    worked out bugs with tutor

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,8 +11,12 @@ app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.use(routes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log('API server running on port ${PORT}!');
+        console.log(`API server running on port ${PORT}!`);
     });
 });
